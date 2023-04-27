a74895bdfc7e03b9797f5eff18840dba090eb3e0
Yinan Feng <53467081+1041267458@users.noreply.github.com>
2023-04-27 10:22:49 -0400

    add new table

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title("Hello World")
 
 st.write("Here's our first attempt at using data to create a table:")
 
 st.text("This is some text.")
+
+st.markdown("This is some markdown.")
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/')
+#DATA_URL = ('https://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+data = pd.read_csv(DATA_URL, nrows=10)
+st.write(data)
